85bb82ef3faf7a38432258ab0a25cbf68703c480
Jose Javier Romero Rivas <jjrr1984@gmail.com>
2022-04-22 23:13:45 +0200

    - Binding elements in main activity - Importing Slider class - Starting to implement event listener for Slider

--- app/src/main/java/com/example/workintime/MainActivity.kt
+++ app/src/main/java/com/example/workintime/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.workintime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.workintime.databinding.ActivityMainBinding
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //  Listener for changes in slider
+        binding.initialHoursSlider.addOnChangeListener()
+    }
+
+    private fun updateHoursText(hours: Float){
+        binding.hoursTextView.setText(hours.toString())
     }
 }
